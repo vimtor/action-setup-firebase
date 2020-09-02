4f8b6cadfdb033270acf470280ffe86ee2dbf7a4
Victor Navarro <victor@vnavarro.dev>
2020-09-02 21:39:59 +0200

    test: check firebase tools is installed

--- tests/enviroment.ts
+++ tests/enviroment.ts
@@ -1,7 +1,16 @@
+import * as commandExists from 'command-exists';
+
 test('environment variable was set correctly', () => {
   const token = process.env.FIREBASE_TOKEN;
 
   expect(token).not.toBeUndefined();
   expect(token).not.toBeNull();
   expect(token).toBe('banana');
+});
+
+test('firebase tools cli is installed', () => {
+  commandExists('firebase', (error, exists) => {
+    expect(error).toBeNull();
+    expect(exists).toBe(true);
+  });
 });
