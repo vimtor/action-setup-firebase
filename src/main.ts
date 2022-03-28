83da993e06d5665856645e46e97a1d69814bda85
Simen A. W. Olsen <so@bjerk.io>
2022-03-28 09:28:14 +0200

    feat: FIREBASE_TOKEN is not mandatory
    Firebase can use Google application default authentication, which means you don't need FIREBASE_TOKEN.
    
    See https://github.com/google-github-actions/auth

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,10 @@ const run = async () => {
   const token = core.getInput('firebase-token')
   const os = process.env.RUNNER_OS
 
-  if (!token) {
-    throw new Error('Missing mandatory input: firebase-token')
+  if (token) {
+    core.exportVariable('FIREBASE_TOKEN', token)
+    core.info('Exported environment variable FIREBASE_TOKEN')
   }
-
-  core.exportVariable('FIREBASE_TOKEN', token)
-  core.info('Exported environment variable FIREBASE_TOKEN')
 
   if (await commandExists('npm')) {
     core.info('Detected NPM installation')
